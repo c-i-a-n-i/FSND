fcdfe3e1c037c789d9369e446416513ba46a5e14
ifani <ifeatu96@gmail.com>
2022-01-23 07:18:59 -0800

    setting up

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,11 +1,10 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-import json
 
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
-  database_path = database_path.replace("postgres://", "postgresql://", 1)
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
 
 db = SQLAlchemy()
 
@@ -13,6 +12,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,19 +26,21 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+class Person(db.Model):
+    __tablename__ = 'People'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    catchphrase = Column(String)
+
+    def __init__(self, name, catchphrase=""):
+        self.name = name
+        self.catchphrase = catchphrase
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'catchphrase': self.catchphrase}
